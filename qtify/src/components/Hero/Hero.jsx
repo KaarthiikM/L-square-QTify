fbaa35704b530ade34e1ce234a162d41832bfbb1
KaarthiikM <kaarthiikmr001@gmail.com>
2024-01-29 17:30:26 +0530

    updated the hero banner

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -4,14 +4,13 @@ import styles from './Hero.module.css'
 
 const Hero = () => {
   return (
-    <div>
-        <div className={styles.hero}>
+    <div className={styles.hero}>
+        <div>
             <h1>100 Thousand Songs, ad-free</h1><br/>
             <h1>Over thousands podcast episodes</h1>
-            <img src={HeroImage} alt="headphones" width={212}/>
         </div>
         <div>
-            
+            <img src={HeroImage} alt="headphones" width={212}/>
         </div>
     </div>
   )
