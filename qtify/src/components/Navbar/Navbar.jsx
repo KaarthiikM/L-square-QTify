7c616a90a2f9154ccca33874a77009ff12953986
KaarthiikM <kaarthiikmr001@gmail.com>
2024-01-29 12:35:19 +0530

    updated

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -3,12 +3,13 @@ import styles from './Navbar.module.css'
 import Logo from '../Logo/Logo'
 import Button from '../Button/Button'
 import Search from '../Search/Search'
+import LogoImage from '../../assets/logoImage.png'
 
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-        <Logo/>
+        <div className={styles.logoDiv}><img  src={LogoImage} alt="logo" width={67}/></div>
         <Search placeholder="Search a album of your choice"/>
         <Button text="Give Feedback"/>
     </nav>
