2621dc6686d779dfd8ee665cdb95888f4fb8059d
KaarthiikM <kaarthiikmr001@gmail.com>
2024-01-31 21:36:57 +0530

    new albums and top albums

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,17 +1,20 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import { fetchTopAlbums } from "./api/api";
+import { fetchNewAlbums, fetchTopAlbums } from "./api/api";
 import { useState , useEffect } from "react";
 import Section from "./components/Section/Section";
 import styles from './App.module.css'
 
 function App() {
-  const [topAlbumsData,settopAlbumsData] = useState([]);
+  const [topAlbumsData, settopAlbumsData] = useState([]);
+  const [newAlbumsData, setNewAlbumsData] = useState([]);
 
   const generateTopAlbums =async()=>{
     try{
       const data = await fetchTopAlbums();
       settopAlbumsData(data)
+      const newdata =await fetchNewAlbums();
+      setNewAlbumsData(newdata)
     }catch(err){
       console.error(err)
     }
@@ -27,7 +30,7 @@ function App() {
       <Hero/>
       <div className={styles.sectionWrapper}>
       <Section data={topAlbumsData} title="Top Albums" type="album"/>
-      <Section data={topAlbumsData} title="New Albums" type="album"/>
+      <Section data={newAlbumsData} title="New Albums" type="album"/>
       </div>
     </div>
   );
--- qtify/src/api/api.js
+++ qtify/src/api/api.js
@@ -12,7 +12,16 @@ export const fetchTopAlbums = async()=>{
     }
 }
 
-export const fetchSongs = async()=>{
+export const fetchNewAlbums = async () => {
+	try {
+		const response = await axios.get(`${BACKEND_ENDPOINT}/albums/new`);
+		return response.data;
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+export const fetchAllSongs = async()=>{
     try{
         const res = await axios.get(`${BACKEND_ENDPOINT}/songs`);
         return res.data
