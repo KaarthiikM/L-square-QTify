2630835ac1f56b28086b794737104a8c53b76a33
KaarthiikM <kaarthiikmr001@gmail.com>
2024-01-31 15:33:48 +0530

    collapse added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,36 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import { fetchTopAlbums } from "./api/api";
+import { useState , useEffect } from "react";
+import Card from "./components/Card/Card";
+import Section from "./components/Section/Section";
 
 function App() {
+  const [topAlbumsData,settopAlbumsData] = useState([]);
+
+  const generateTopAlbums =async()=>{
+    try{
+      const data = await fetchTopAlbums();
+      settopAlbumsData(data)
+    }catch(err){
+      console.error(err)
+    }
+  }
+
+  useEffect(()=>{
+    generateTopAlbums();
+  },[])
+
   return (
     <div>
       <Navbar/>
       <Hero/>
+      {/* {
+        topAlbumsData.map((topAlbum)=>(
+          <Card data={topAlbum} type="album" key={topAlbum.id}/>
+        ))
+      } */}
+      <Section data={topAlbumsData} title="Top Albums" type="album"/>
     </div>
   );
 }
